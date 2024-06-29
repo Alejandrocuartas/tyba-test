bdcdf32b5138c44384296f13b44651ed6e42242d
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2024-06-29 16:51:13 -0500

    fix: configure ssh for local and for lambda

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,16 +1,21 @@
 import { Sequelize } from 'sequelize';
 import dbConfig from '../config';
 
+let ssl: undefined | { require: boolean; rejectUnauthorized: boolean } = undefined;
+if (process.env.IS_LAMBDA_ENV) {
+    ssl = {
+        require: true,
+        rejectUnauthorized: false,
+    };
+}
+
 const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
     host: dbConfig.host,
     dialect: dbConfig.dialect,
     port: dbConfig.port,
     logging: console.log,
     dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
+        ssl,
     },
 });
 
